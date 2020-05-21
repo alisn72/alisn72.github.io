b6e61ca2c70983c2ef049f2eae76e80940f55c9d
sabzinezhad <fama234@gmail.com>
2020-05-21 11:21:10 +0430

    Initial_Commit

---
+++ precache-manifest.fe0ad81ba1ae39b960522f3f1ef5ca92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6395da594a29cecd5fff157ea02b511a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a30d40186952a45254d",
+    "url": "/static/css/2.2e872fcd.chunk.css"
+  },
+  {
+    "revision": "647cf27b749b848dd04f",
+    "url": "/static/css/main.6ab76fbb.chunk.css"
+  },
+  {
+    "revision": "9a30d40186952a45254d",
+    "url": "/static/js/2.1aef488e.chunk.js"
+  },
+  {
+    "revision": "572cb266d4e70c65da21f06e2ade4c91",
+    "url": "/static/js/2.1aef488e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647cf27b749b848dd04f",
+    "url": "/static/js/main.4e448bf8.chunk.js"
+  },
+  {
+    "revision": "bd4c681accc02400a9be",
+    "url": "/static/js/runtime-main.f539de1a.js"
+  },
+  {
+    "revision": "e8ef20357ee97e45a8d93cd1e0d1330f",
+    "url": "/static/media/IRANSansWeb.e8ef2035.woff2"
+  }
+]);
